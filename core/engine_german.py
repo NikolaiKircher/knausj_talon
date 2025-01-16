5e31bee66af9f102684c7dac36960d1ec963bf44
Nikolai Kircher <nikolai.kircher@gmail.com>
2025-01-16 20:53:47 +0100

    try to fix engine_german

--- core/engine_german.py
+++ core/engine_german.py
@@ -5,7 +5,6 @@ from talon.engines.w2l import W2lEngine, W2lModelInfo
 model_path = TALON_HOME / 'w2l' / 'conformer-d-de'
 with open(model_path / 'ort.patch', 'rb') as f:
     ort_patch = f.read()
-
 info = W2lModelInfo(
     id           = 'd-small-de',
     name         = '(2023-01-07)',
@@ -15,8 +14,8 @@ info = W2lModelInfo(
     trie_path    = str(model_path / 'lexicon_flat.bin'),
     ort_patch    = ort_patch,
     flags        = '',
+    model_format = '',
 )
-
 w2l_de = W2lEngine(info=info, debug=True)
 speech_system.add_engine(w2l_de)
 
